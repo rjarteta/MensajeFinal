9670b375891d32c17d6008035b98a7425d90960b
Ricardo José Arteta Romero <recontrapanche@gmail.com>
2010-02-05 22:27:14 -0500

    error en entrada

--- app/controllers/entradas_controller.rb
+++ app/controllers/entradas_controller.rb
@@ -61,24 +61,24 @@ class EntradasController < ApplicationController
 
 private
   def enlazar_archivos(archivos_from_params, entrada)
-    archivos_entradas_ids = []
-    archivos = {}; archivos ||= archivos_from_params
-    archivos.each do |key, value|
-      if key =~ /archivo_/
-        archivo_id = value.to_i
-        archivo_entrada = ArchivoEntrada.find_or_create_by_archivo_id_and_entrada_id(archivo_id,entrada.id)
-        archivo_entrada.orden = archivos["orden_#{archivo_id}"].to_i
-        archivo_entrada.muestra_cont = archivos["muestra_contenido_#{archivo_id}"] && archivos["muestra_contenido_#{archivo_id}"].to_i == 1 ? true : false
-        archivo_entrada.save!
-        archivos_entradas_ids << archivo_entrada.id
-      end
-    end
-    options_for_delete = ["entrada_id = ?", entrada.id]
-    unless archivos_entradas_ids.empty?
-      options_for_delete[0] += " AND id NOT IN (?)"
-      options_for_delete << archivos_entradas_ids
-    end
-    ArchivoEntrada.delete_all(options_for_delete)
+#     archivos_entradas_ids = []
+#     archivos = {}; archivos ||= archivos_from_params;
+#     archivos.each do |key, value|
+#       if key =~ /archivo_/
+#         archivo_id = value.to_i
+#         archivo_entrada = ArchivoEntrada.find_or_create_by_archivo_id_and_entrada_id(archivo_id,entrada.id)
+#         archivo_entrada.orden = archivos["orden_#{archivo_id}"].to_i
+#         archivo_entrada.muestra_cont = archivos["muestra_contenido_#{archivo_id}"] && archivos["muestra_contenido_#{archivo_id}"].to_i == 1 ? true : false
+#         archivo_entrada.save!
+#         archivos_entradas_ids << archivo_entrada.id
+#       end
+#     end
+#     options_for_delete = ["entrada_id = ?", entrada.id]
+#     unless archivos_entradas_ids.empty?
+#       options_for_delete[0] += " AND id NOT IN (?)"
+#       options_for_delete << archivos_entradas_ids
+#     end
+#     ArchivoEntrada.delete_all(options_for_delete)
   end
    
 end
